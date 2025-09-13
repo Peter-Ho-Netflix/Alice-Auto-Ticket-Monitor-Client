b690065758ac8cfc3534394b8f92f8a73b6d5466
Peter-Ho-Netflix <diannaobingdukb@aliyun.com>
2025-09-13 11:04:21 +0800

    Add Material-UI components and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
 
 function App() {
   return (
@@ -11,12 +12,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Button variant="contained">Learn React</Button>
         </a>
       </header>
     </div>
